f058c44ffdbfd900cb7986e3501c3a47b2c379bf
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-06-12 08:16:16 +0300

    some improvement

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("./startup/logger");
+
 const app = express();
 
 app.use(cors());
@@ -8,7 +9,7 @@ app.use(cors());
 // add routes
 require("./startup/routes")(app);
 
-// add listner
+// Launch server
 const port = process.env.PORT || 5000;
 app.listen(port, async () => {
   const dbConnected = await require("./startup/db")();
--- startup/db.js
+++ startup/db.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const logger = require("./logger");
 require("dotenv").config();
 
-module.exports = async () => await mongoose.connect(process.env.DB_URIi);
+module.exports = async () => {
+  const connected = await mongoose.connect(process.env.DB_URI);
+  logger.info("Database is connected successfully!");
+  return connected;
+};

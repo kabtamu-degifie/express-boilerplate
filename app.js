24c4dbaef6e1a2b37a9288cc2dc58e98841ecd7c
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-06-13 15:39:20 +0300

    added express-jwt, cookies-parser and other improvements

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const { port } = require("./config/vars");
+const cookieParser = require("cookie-parser");
+const { expressjwt: jwt } = require("express-jwt");
+
+const { jwt_key } = require("./config/vars");
+
+const routes = require("./config/routes");
+
 const mongoose = require("./config/mongoose");
-const logger = require("./config/logger");
 
 const v1Router = require("./routes/v1.router");
 
@@ -13,11 +19,17 @@ mongoose.connect();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  jwt({ secret: jwt_key, algorithms: ["HS256"] }).unless({
+    path: routes.public,
+  })
+);
 
 // Diffrent version of routers
 app.use("/v1", v1Router);
 
-// Launch server
-app.listen(port, () => {
-  logger.info(`Listening on port: ${port}`);
-});
+module.exports = app;

fc9a6f9c21a41ebfcf2c582a6ab0920a79a63a03
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-06-14 12:07:35 +0300

    send all permissions on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,8 +7,25 @@ const login = async (req, res) => {
   }).populate({ path: "roles", populate: { path: "permissions" } });
 
   if (user && user.verifyPassword(req.body.password)) {
+    // Maping roles => find permissions inside role => combine them and form set
+    let permissions = user.roles.reduce((previousRole, currentRole) => {
+      return [
+        ...previousRole,
+        ...currentRole.permissions.map((permission) => permission.name),
+      ];
+    }, []);
+    user._doc.permissions = Array.from(
+      new Set([
+        ...user._doc.permissions.map((permission) => permission.name),
+        ...permissions,
+      ])
+    );
+
     const token = user.generateToken();
-    res.send({ ..._.pick(user, ["_id", "username", "active"]), token });
+    res.send({
+      ..._.pick(user, ["_id", "username", "active", "permissions"]),
+      token,
+    });
   } else {
     return res.status(400).send("Invalid username / password.");
   }

42410e984e60ad3429faf653eb8e232a16c6b5b8
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-07-07 18:13:57 +0300

    fix auth middleware issue

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,24 @@
 const hasPermission = (...permissions) => {
   return (req, res, next) => {
     const user = req.auth;
-    if (req.auth) {
-      try {
-        if (user) {
-          permissions.forEach((permission) => {
-            if (!user.data.permissions.includes(permission)) {
-              errors.push(`You don't have ${permission} permission`);
-            }
-          });
-
-          if (errors.length === 0) {
-            next();
-          } else {
-            throw new Error("You dont have permission to perform this action.");
-          }
-        }
-      } catch (error) {
-        res.status(401).send([...errors, error]);
-      }
-    } else {
-      res.status(401).send("No token is provided.");
-    }
     const errors = [];
+    try {
+      if (user) {
+        permissions.forEach((permission) => {
+          if (!user.data.permissions.includes(permission)) {
+            errors.push(`You don't have ${permission} permission`);
+          }
+        });
+
+        if (errors.length === 0) {
+          next();
+        } else {
+          throw new Error("You dont have permission to perform this action.");
+        }
+      }
+    } catch (error) {
+      res.status(401).send([...errors, error]);
+    }
   };
 };
 

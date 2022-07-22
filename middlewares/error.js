ed6465878cf647cee62ff325a93416aa22ed63ba
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-07-22 07:23:39 +0300

    update error middleware

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,11 +1,9 @@
 module.exports = (error, req, res, next) => {
   if (error.name === "UnauthorizedError") {
     res.status(401).send(error.message);
-  }
-
-  if (error.name === "CastError" && error.kind === "ObjectId") {
+  } else if (error.name === "CastError" && error.kind === "ObjectId") {
     res.status(401).send("Invalid ObjectId format.");
+  } else {
+    res.status(500).send("Something went wrong!");
   }
-
-  res.status(500).send("Something went wrong!");
 };
